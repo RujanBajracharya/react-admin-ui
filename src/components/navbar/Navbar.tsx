0b7aa2defdbf0b2981c998f7bb40218aaac658cd
Rujan-Bajracharya <rujan.bajracharya123@gmail.com>
2023-09-20 15:47:43 +0545

    responsive grids ig

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,9 +8,9 @@ export const Navbar = () => {
         <span>lamadev</span>
       </div>
       <div className="icons">
-        <img src="search.svg" alt="" />
-        <img src="app.svg" alt="" />
-        <img src="expand.svg" alt="" />
+        <img src="search.svg" alt="" className='icon'/>
+        <img src="app.svg" alt="" className='icon'/>
+        <img src="expand.svg" alt="" className='icon'/>
 
         <div className="notification">
           <img src="notifications.svg" alt="" />
